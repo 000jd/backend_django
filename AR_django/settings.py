a77e0e4906ac3f2f80d51fe517f781507ac06bf4
Joydip Das <48018188+000jd@users.noreply.github.com>
2023-04-15 18:58:04 +0530

    Update settings.py

--- AR_django/settings.py
+++ AR_django/settings.py
@@ -24,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$0n&v7#)iy*#@_)#2__37^@^4aku0m8&97p7*me0*v4(sa0&e0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
-ALLOWED_HOSTS = ["backenddjango-production.up.railway.app", ]
+ALLOWED_HOSTS = ["backenddjango-production.up.railway.app", "https://backenddjango-production.up.railway.app" ]
 
-CSRF_TRUSTED_ORIGINS = ['https://backenddjango-production.up.railway.ap',
-                        'https://www.backenddjango-production.up.railway.ap']
+CSRF_TRUSTED_ORIGINS = ['https://backenddjango-production.up.railway.app',
+                        'https://www.backenddjango-production.up.railway.app']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
